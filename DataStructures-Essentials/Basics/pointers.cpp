80c7f4d26534e6838f5a5fa63911b5abcf697244
Phoenix <anmolkhanna2003@gmail.com>
2024-07-17 13:10:26 +0530

    Searching a 2D Matrix

---
+++ DataStructures-Essentials/Basics/pointers.cpp
@@ -0,0 +1,8 @@
+//
+//  pointers.cpp
+//  DataStructures-Essentials
+//
+//  Created by Anmol Khanna on 17/07/24.
+//
+
+#include <stdio.h>
---
+++ DataStructures-Essentials/LeetCode/Search-a-2D-Matric.cpp
@@ -0,0 +1,8 @@
+//
+//  Search-a-2D-Matric.cpp
+//  DataStructures-Essentials
+//
+//  Created by Anmol Khanna on 17/07/24.
+//
+
+#include <stdio.h>
