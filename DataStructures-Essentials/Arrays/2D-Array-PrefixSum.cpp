ac81bda317730bda33a71aabd7c8c15c3c170e62
Phoenix <anmolkhanna2003@gmail.com>
2024-07-18 08:28:14 +0530

    2D Sum Search

--- DataStructures-Essentials/Arrays/2D-Array-PrefixSum.cpp
+++ DataStructures-Essentials/Arrays/2D-Array-PrefixSum.cpp
@@ -13,3 +13,24 @@ int findTheSum(int** arr, int rows, int columns, int* dimension){
     }
     return 0;
 }
+
+class NumMatrix {
+    vector<vector<int>> sumMatrix;
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        int rows = matrix.size()+1;
+        int cols = matrix[0].size()+1;
+        sumMatrix = vector<vector<int>>(rows,vector<int>(cols));
+        for(int i = 1;i<rows;i++){
+            for(int j = 1; j<cols;j++){
+                sumMatrix[i][j] = matrix[i-1][j-1] + sumMatrix[i-1][j] + sumMatrix[i][j-1] - sumMatrix[i-1][j-1];
+            }
+        }
+        
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return sumMatrix[row2+1][col2+1] - sumMatrix[row1][col2+1] - sumMatrix[row2+1][col1] + sumMatrix[row1][col1]
+    }
+};
+
